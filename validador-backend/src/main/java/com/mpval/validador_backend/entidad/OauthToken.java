352ff08662196b62d879f4b69de6487b5b98cc5d
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-25 15:59:32 -0300

    Update OauthToken.java

--- validador-backend/src/main/java/com/mpval/validador_backend/entidad/OauthToken.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/entidad/OauthToken.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "oauth_tokens_tb")
 public class OauthToken {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
