015bc6e6cc687fcb20f48f1b2660d10e04f1ec88
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-25 15:19:45 -0300

    Creación de la base de datos

---
+++ validador-backend/base de datos/creacionDB.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS Mercado_Pago_Validador_DB;
+USE Mercado_Pago_Validador_DB;
+
+CREATE TABLE usuarios_tb (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre_de_usuario VARCHAR(50) NOT NULL UNIQUE,
+    correo VARCHAR(100) NOT NULL UNIQUE,
+    contrasena VARCHAR(255) NOT NULL,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    expiracion_suscripcion DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE oauth_tokens_tb (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    access_token VARCHAR(500) NOT NULL,
+    refresh_token VARCHAR(500) NOT NULL,
+    public_key VARCHAR(255),
+    user_id BIGINT,-- ID del vendedor en MP
+    expires_at DATETIME NOT NULL,
+    live_mode BOOLEAN DEFAULT false,
+    usuario_id BIGINT NOT NULL,-- id de usuario de BD propia
+    CONSTRAINT fk_oauth_usuario
+        FOREIGN KEY (usuario_id)
+        REFERENCES usuarios_tb(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE state_oauth_tb (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    state VARCHAR(255) NOT NULL,
+    usuario_id BIGINT NOT NULL,
+    creado DATETIME NOT NULL
+);
+
