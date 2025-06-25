31a6dfebf86b376571ee179e3016960db1a415ab
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-25 15:59:37 -0300

    Update StateOauth.java

--- validador-backend/src/main/java/com/mpval/validador_backend/entidad/StateOauth.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/entidad/StateOauth.java
@@ -1,4 +1,27 @@
 package com.mpval.validador_backend.entidad;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "state_oauth_tb")
 public class StateOauth {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String state;
+    private Long usuarioId;
+    private LocalDateTime creado;
 }
