02aabdabc5ffdfbc023820ac7fea20e02e7c4b37
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-26 14:42:06 -0300

    Create StateOauthRepositorio.java

---
+++ validador-backend/src/main/java/com/mpval/validador_backend/repositorio/StateOauthRepositorio.java
@@ -0,0 +1,8 @@
+package com.mpval.validador_backend.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mpval.validador_backend.entidad.StateOauth;
+
+public interface StateOauthRepositorio extends JpaRepository<StateOauth, Long> {
+}
