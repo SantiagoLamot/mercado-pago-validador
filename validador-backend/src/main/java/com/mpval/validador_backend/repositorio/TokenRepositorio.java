ba598e82443d8c8f2f13206bf173338e546bd186
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-27 15:03:32 -0300

    Update TokenRepositorio.java

--- validador-backend/src/main/java/com/mpval/validador_backend/repositorio/TokenRepositorio.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/repositorio/TokenRepositorio.java
@@ -1,8 +1,14 @@
 package com.mpval.validador_backend.repositorio;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mpval.validador_backend.entidad.Token;
 
-public interface TokenRepositorio extends JpaRepository<Token, Long>{
+public interface TokenRepositorio extends JpaRepository<Token, Long> {
+    Optional<Token> findByToken(String jwt);
+
+List<Token> findByUsuarioIdAndExpiredFalseAndRevokedFalse(Long id);
 }
