097c753840d181ffc0cfd30b1063fb2e40a83ab0
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-27 15:02:56 -0300

    Create AuthServicio.java

---
+++ validador-backend/src/main/java/com/mpval/validador_backend/servicio/AuthServicio.java
@@ -0,0 +1,10 @@
+package com.mpval.validador_backend.servicio;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServicio {
+}
