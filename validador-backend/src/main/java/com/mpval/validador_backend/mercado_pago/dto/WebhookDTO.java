578c2270bb85b86edc7e318b41f885ab3e2684fc
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-28 01:34:03 -0300

    Create WebhookDTO.java

---
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/dto/WebhookDTO.java
@@ -0,0 +1,23 @@
+package com.mpval.validador_backend.mercado_pago.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class WebhookDTO {
+    private String action;
+    private String api_version;
+    private WebhookData data;
+    private String date_created;
+    private String id;
+    private boolean live_mode;
+    private String type;
+    private Long user_id;
+
+    @Data
+    public static class WebhookData {
+        private String id;
+    }
+}
