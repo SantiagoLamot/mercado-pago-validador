8bbcb3ee08e934976f02f252ac27be9ae07e6447
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-28 01:34:22 -0300

    Update StateOauthRepository.java

--- validador-backend/src/main/java/com/mpval/validador_backend/Mercado_Pago/repository/StateOauthRepository.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/Mercado_Pago/repository/StateOauthRepository.java
@@ -1,8 +1,11 @@
-package com.mpval.validador_backend.Mercado_Pago.repository;
+package com.mpval.validador_backend.mercado_pago.repository;
+
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.mpval.validador_backend.Mercado_Pago.entity.StateOauth;
+import com.mpval.validador_backend.mercado_pago.entity.StateOauth;
 
 public interface StateOauthRepository extends JpaRepository<StateOauth, Long> {
+    Optional<StateOauth> findByState(String state);
 }
