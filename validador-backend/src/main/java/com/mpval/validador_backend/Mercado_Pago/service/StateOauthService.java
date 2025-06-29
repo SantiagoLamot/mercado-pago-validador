ddcad2ef444a348fd20fdf7464c40514ec3a84ae
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-29 10:10:03 -0300

    Update StateOauthService.java

--- validador-backend/src/main/java/com/mpval/validador_backend/Mercado_Pago/service/StateOauthService.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/Mercado_Pago/service/StateOauthService.java
@@ -1,14 +1,26 @@
-package com.mpval.validador_backend.Mercado_Pago.service;
+package com.mpval.validador_backend.mercado_pago.service;
 
 import org.springframework.stereotype.Service;
 
-import com.mpval.validador_backend.Mercado_Pago.repository.StateOauthRepository;
+import com.mpval.validador_backend.mercado_pago.entity.StateOauth;
+import com.mpval.validador_backend.mercado_pago.repository.StateOauthRepository;
 
 @Service
 public class StateOauthService {
-    private final StateOauthRepository stateOauthRepositorio;
+    private final StateOauthRepository stateOauthRepository;
 
-    public StateOauthService(StateOauthRepository sr){
-        this.stateOauthRepositorio = sr;
+    public StateOauthService(StateOauthRepository stateOauthRepository) {
+        this.stateOauthRepository = stateOauthRepository;
+    }
+
+    public void guardarStateOauth(Long idUsuarioLogueado, String state) {
+        StateOauth entity = new StateOauth(idUsuarioLogueado, state);
+        stateOauthRepository.save(entity);
+    }
+
+    public Long obtenerIdUsuarioPorState(String state) {
+        return stateOauthRepository.findByState(state)
+                .orElseThrow(() -> new RuntimeException("state no encontrado"))
+                .getUsuarioId();
     }
 }
