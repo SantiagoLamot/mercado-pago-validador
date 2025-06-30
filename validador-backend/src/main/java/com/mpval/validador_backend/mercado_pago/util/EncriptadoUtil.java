9c65a0f4dd9155a99a9ca2f57a119c2a09e40dcc
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-30 15:10:36 -0300

    Update EncriptadoUtil.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/util/EncriptadoUtil.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/util/EncriptadoUtil.java
@@ -1,5 +1,10 @@
 package com.mpval.validador_backend.mercado_pago.util;
 
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +16,27 @@ public class EncriptadoUtil {
     @Value("${secretKeyEncriptar}")
     private String secretKeyEncriptar;
 
+    public String encriptar(String plainText) {
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(secretKeyEncriptar.getBytes(), algoritmoEncriptar);
+            Cipher cipher = Cipher.getInstance(algoritmoEncriptar);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error al cifrar el token", e);
+        }
+    }
+
+    public String desencriptar(String encryptedText) {
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(secretKeyEncriptar.getBytes(), algoritmoEncriptar);
+            Cipher cipher = Cipher.getInstance(algoritmoEncriptar);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+            return new String(decryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error al descifrar el token", e);
+        }
+    }
 }
