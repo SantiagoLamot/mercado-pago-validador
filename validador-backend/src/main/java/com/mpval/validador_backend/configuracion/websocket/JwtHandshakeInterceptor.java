b8d565d7a8e91e26c309444e1973cc5fce2946f5
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-01 19:53:19 -0300

    Create JwtHandshakeInterceptor.java

---
+++ validador-backend/src/main/java/com/mpval/validador_backend/configuracion/websocket/JwtHandshakeInterceptor.java
@@ -0,0 +1,6 @@
+package com.mpval.validador_backend.configuracion.websocket;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+public class JwtHandshakeInterceptor implements HandshakeInterceptor {
+
+}
