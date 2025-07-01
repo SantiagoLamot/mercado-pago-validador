4d6ede2e8785401f8bc6d8e99a5313401b848f05
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-01 14:39:11 -0300

    Update ChatController.java

--- validador-backend/src/main/java/com/mpval/validador_backend/webSocket/controller/ChatController.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/webSocket/controller/ChatController.java
@@ -6,25 +6,11 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
-import com.mpval.validador_backend.webSocket.dto.PagoNotificacionDTO;
-
-import lombok.Data;
-
 @Controller
 public class ChatController {
     @MessageMapping("/app/chat")
     @SendToUser("/queue/mensajes")
-    public PagoNotificacionDTO manejarMensaje(Mensaje mensaje, Principal principal) {
-        return PagoNotificacionDTO.builder()
-                .mensaje("Pago exitoso")
-                .monto(123.45)
-                .nombreComprador(principal.getName()) // quien está logueado
-                .build();
-    }
-
-    @Data
-    public static class Mensaje {
-        private String texto;
-        private String fecha;
+    public String manejarMensaje(Principal principal) {
+        return "Se recibio mensaje del frontend en el backend";
     }
 }
