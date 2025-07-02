97cd2d570c9e6350331e924472853e56a43535e0
FacundoOrona <fcndorona@gmail.com>
2025-07-02 20:47:35 -0300

    Home design

--- front/validador-frontend/src/pages/Home.jsx
+++ front/validador-frontend/src/pages/Home.jsx
@@ -1,2 +1,14 @@
-const Home = () => <h1>Menú Principal</h1>
+import fondo from '../assets/fondo.jpg'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="home-background">
+      <div className="overlay">
+        <h1 className="text-white">Bienvenido al Menú Principal</h1>
+      </div>
+    </div>
+  )
+}
+
 export default Home
