1447fe10bdd249e3505e0becd0c5eccdb8a6578a
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-04 14:41:08 -0300

    nuevos directorios backend

---
+++ validador-backend/src/test/java/com/mpval/validador_backend/ValidadorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpval.validador_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidadorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
