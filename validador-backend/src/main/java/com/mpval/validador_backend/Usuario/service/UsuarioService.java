92bcc89385c9d9e4c2ead72307a1624f8790b27c
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-05 19:41:46 -0300

    Update UsuarioService.java

--- validador-backend/src/main/java/com/mpval/validador_backend/Usuario/service/UsuarioService.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/Usuario/service/UsuarioService.java
@@ -1,12 +1,12 @@
-package com.mpval.validador_backend.usuario.service;
+package com.mpval.validador_backend.Usuario.service;
 
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.mpval.validador_backend.Usuario.entity.Usuario;
+import com.mpval.validador_backend.Usuario.repository.UsuarioRepository;
 import com.mpval.validador_backend.mercado_pago.service.StateOauthService;
-import com.mpval.validador_backend.usuario.entity.Usuario;
-import com.mpval.validador_backend.usuario.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
