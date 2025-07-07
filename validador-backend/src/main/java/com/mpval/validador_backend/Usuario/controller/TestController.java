fa260312c1cf3282f3c97db714a2b2c14d84e921
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-06 21:37:48 -0300

    Create TestController.java

---
+++ validador-backend/src/main/java/com/mpval/validador_backend/Usuario/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.mpval.validador_backend.Usuario.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "API funcionando correctamente";
+    }
+}
