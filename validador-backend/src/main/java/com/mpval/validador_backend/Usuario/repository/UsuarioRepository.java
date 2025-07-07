1c6036f6c7edceea537c7eb2a397ba81a85f4c69
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-06 21:37:41 -0300

    Update UsuarioRepository.java

--- validador-backend/src/main/java/com/mpval/validador_backend/Usuario/repository/UsuarioRepository.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/Usuario/repository/UsuarioRepository.java
@@ -3,10 +3,11 @@ package com.mpval.validador_backend.Usuario.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mpval.validador_backend.Usuario.entity.Usuario;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
     Optional<Usuario> findByNombreDeUsuario(String nombreDeUsuario);
 }
