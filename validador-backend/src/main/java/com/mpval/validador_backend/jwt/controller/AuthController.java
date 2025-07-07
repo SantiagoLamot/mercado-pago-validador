396d3862406307ffa0ccd1118a1dc640ad358c3a
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-07 16:40:28 -0300

    Update AuthController.java

--- validador-backend/src/main/java/com/mpval/validador_backend/jwt/controller/AuthController.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/jwt/controller/AuthController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class AuthController {
 
