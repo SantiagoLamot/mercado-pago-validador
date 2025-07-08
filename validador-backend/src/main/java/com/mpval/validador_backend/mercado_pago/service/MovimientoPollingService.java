4df639220ec3ffae95610ac796aa2d857d33f00b
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-08 20:53:16 -0300

    Update MovimientoPollingService.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/service/MovimientoPollingService.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/service/MovimientoPollingService.java
@@ -1,5 +1,14 @@
 package com.mpval.validador_backend.mercado_pago.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class MovimientoPollingService {
     
+    private final RestTemplate restTemplate;
+
+    public MovimientoPollingService(RestTemplate r){
+        this.restTemplate = r;
+    }
 }
