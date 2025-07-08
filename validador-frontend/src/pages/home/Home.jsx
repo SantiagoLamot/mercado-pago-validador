a4c3a7d04f3dce37477bed68cc69cefafbc40334
FacundoOrona <fcndorona@gmail.com>
2025-07-08 17:27:05 -0300

    Create Home.jsx

---
+++ validador-frontend/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import fondo from '../../assets/fondo.jpg';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-background">
+      <div className="overlay">
+        <h1 className="text-white">Bienvenido al Menú Principal</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
