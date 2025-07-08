566b043539e603e0360b21b6f5a2106ea5df8b9f
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-08 20:47:21 -0300

    Update ValidadorBackendApplication.java

--- validador-backend/src/main/java/com/mpval/validador_backend/ValidadorBackendApplication.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/ValidadorBackendApplication.java
@@ -2,8 +2,10 @@ package com.mpval.validador_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ValidadorBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ValidadorBackendApplication.class, args);
