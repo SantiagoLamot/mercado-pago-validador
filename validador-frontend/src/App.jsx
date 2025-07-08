92d43bfe9af682a7f28be9cce8293fd88c2dcb13
FacundoOrona <fcndorona@gmail.com>
2025-07-08 17:24:15 -0300

    Update App.jsx

--- validador-frontend/src/App.jsx
+++ validador-frontend/src/App.jsx
@@ -1,8 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Register from './pages/Register'
-import Login from './pages/Login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+import Suscripcion from './pages/suscripcion/Suscripcion';
+import Notificaciones from './pages/notificaciones/Notificaciones';
+import Oauth from './pages/oauth/Oauth';
 
 function App() {
   return (
@@ -12,10 +15,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/suscripcion" element={<Suscripcion />} />
+        <Route path="/notificaciones" element={<Notificaciones />} />
+        <Route path="/oauth" element={<Oauth />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
 
