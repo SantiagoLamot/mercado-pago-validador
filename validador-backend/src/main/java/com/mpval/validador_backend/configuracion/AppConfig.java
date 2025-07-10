bcc0bdc639ac5f690f090370c00f1a9e13e743e9
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-10 15:39:23 -0300

    Update AppConfig.java

--- validador-backend/src/main/java/com/mpval/validador_backend/configuracion/AppConfig.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/configuracion/AppConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.mpval.validador_backend.Usuario.entity.Usuario;
 import com.mpval.validador_backend.Usuario.repository.UsuarioRepository;
@@ -58,5 +60,19 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/auth/logout") // para permitor todos "/**"
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // para uso de cookies o tokens
+            }
+        };
     }
 }
