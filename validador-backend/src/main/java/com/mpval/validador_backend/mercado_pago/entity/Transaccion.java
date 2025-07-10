32400e6e182367cc1175873c106aa2ad6348ea78
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-10 16:09:45 -0300

    Update Transaccion.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/entity/Transaccion.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/entity/Transaccion.java
@@ -1,6 +1,15 @@
 package com.mpval.validador_backend.mercado_pago.entity;
 
+import java.time.LocalDateTime;
+
+import com.mpval.validador_backend.Usuario.entity.Usuario;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +19,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Transaccion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String estado;
+
+    private final LocalDateTime fecha = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
 }
