64647f4443fd8b05c7002ed83d08b1f954d72383
FacundoOrona <fcndorona@gmail.com>
2025-07-10 16:30:06 -0300

    Update Home.jsx

--- validador-frontend/src/pages/home/Home.jsx
+++ validador-frontend/src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from 'react';
 import styles from './Home.module.scss';
 
 const Home = () => {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const storedUser = sessionStorage.getItem('username');
+    setUsername(storedUser);
+  }, []);
+
   return (
     <div className={styles.homeBackground}>
       <div className={styles.overlay}>
-        <h1 className={styles.title}>Bienvenido al validador de Mercado Pago</h1>
-        <p className={styles.subtitle}>Asegurate de recibir tus transacciones!</p>
+        <h1 className={styles.title}>Bienvenido {username} al validador de MP</h1>
+        <p className={styles.subtitle}>Asegurate de recibir tus transacciones!.</p>
       </div>
     </div>
   );
