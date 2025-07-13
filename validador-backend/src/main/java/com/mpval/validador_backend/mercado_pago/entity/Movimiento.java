ed82a2f1c411834a046a8eeee91e9e8108f47029
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-13 17:51:04 -0300

    Update Movimiento.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/entity/Movimiento.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/entity/Movimiento.java
@@ -1,9 +1,19 @@
 package com.mpval.validador_backend.mercado_pago.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class Movimiento {
-    
+    private Long id;
+    private String type;
+    private String status;
+    private String detail;
+    private Double amount;
+    private String date_created;
 }
