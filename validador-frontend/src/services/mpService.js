274edba0da4d7208f78d8098880a5c90f14e82de
FacundoOrona <fcndorona@gmail.com>
2025-07-15 16:57:17 -0300

    Update mpService.js

--- validador-frontend/src/services/mpService.js
+++ validador-frontend/src/services/mpService.js
@@ -4,3 +4,8 @@ export const iniciarPagoSuscripcion = async (param) => {
   const response = await api.get(`/pago/suscripcion?param=${encodeURIComponent(param)}`);
   return response.data; // devuelve el link de pago
 };
+
+export const iniciarOauth = async () => {
+  const response = await api.get('/oauth/init');
+  return response.data;
+};
