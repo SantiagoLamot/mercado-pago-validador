d0cefb94e09c78a727ef48551d2e4b2fb94ecb46
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-16 19:46:22 -0300

    Update OauthTokenRepository.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/repository/OauthTokenRepository.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/repository/OauthTokenRepository.java
@@ -1,5 +1,6 @@
 package com.mpval.validador_backend.mercado_pago.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import com.mpval.validador_backend.mercado_pago.entity.OauthToken;
 public interface OauthTokenRepository extends JpaRepository<OauthToken, Long> {
     Optional<OauthToken> findByUsuarioId(Long userId);
     Optional <OauthToken> findByUserId(Long idMp);
+    List<OauthToken> findByAccessTokenIsNotNull();
 }
