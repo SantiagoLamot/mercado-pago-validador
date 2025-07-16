5d6bc8e261cfae0b63b1e0a07e52469f83a9d9fc
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-16 19:47:06 -0300

    Update MovimientoPollingService.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/service/MovimientoPollingService.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/service/MovimientoPollingService.java
@@ -1,7 +1,19 @@
 package com.mpval.validador_backend.mercado_pago.service;
 
+import java.util.List;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.mpval.validador_backend.mercado_pago.dto.MovimientoResponse;
+import com.mpval.validador_backend.mercado_pago.entity.Movimiento;
+import com.mpval.validador_backend.mercado_pago.entity.OauthToken;
+import com.mpval.validador_backend.mercado_pago.repository.OauthTokenRepository;
 
 @Service
 public class MovimientoPollingService {
