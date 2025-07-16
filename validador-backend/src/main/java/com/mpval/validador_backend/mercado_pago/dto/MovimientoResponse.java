a4096a538a540a91542c8779e2ac87226299b1b8
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-15 21:28:11 -0300

    Update MovimientoResponse.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/dto/MovimientoResponse.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/dto/MovimientoResponse.java
@@ -1,5 +1,18 @@
 package com.mpval.validador_backend.mercado_pago.dto;
 
-public class MovimientoResponse {
+import java.util.List;
 
+import com.mpval.validador_backend.mercado_pago.entity.Movimiento;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovimientoResponse {
+    private List<Movimiento> movimientos;
 }
