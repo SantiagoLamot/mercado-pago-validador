bda1c48704ff462a1d607cbf0bb3ee7454b5b483
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-07-16 19:46:08 -0300

    Update MovimientoResponse.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/dto/MovimientoResponse.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/dto/MovimientoResponse.java
@@ -2,17 +2,19 @@ package com.mpval.validador_backend.mercado_pago.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.mpval.validador_backend.mercado_pago.entity.Movimiento;
 
-import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MovimientoResponse {
-    private List<Movimiento> movimientos;
+    @JsonProperty("results")
+    private List<Movimiento> results;
 }
