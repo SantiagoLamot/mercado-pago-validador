f2a04585beb40f6c736b1851a0ff46b12bc53fb6
Santiago Lamot <santiagolamot@hotmail.com>
2025-07-23 15:04:34 -0300

    Update NotificacionService.java

--- validador-backend/src/main/java/com/mpval/validador_backend/webSocket/service/NotificacionService.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/webSocket/service/NotificacionService.java
@@ -3,6 +3,7 @@ package com.mpval.validador_backend.webSocket.service;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import com.mpval.validador_backend.mercado_pago.dto.EstadoUsuarioDTO;
 import com.mpval.validador_backend.webSocket.dto.PagoNotificacionDTO;
 
 @Service
@@ -18,6 +19,14 @@ public class NotificacionService {
         messagingTemplate.convertAndSendToUser(
                 nombreDeUsuario,// nombre del usuario autenticado (del JWT)
                 "/queue/pagos",// destino STOMP que escucha el frontend
+                dto
+        );
+    }
+
+    public void notificarFechaSuscripcionAUsuario(String nombreDeUsuario, EstadoUsuarioDTO dto) {
+        messagingTemplate.convertAndSendToUser(
+                nombreDeUsuario,// nombre del usuario autenticado (del JWT)
+                "/queue/usuario",// destino STOMP que escucha el frontend
                 dto
         );
     }
