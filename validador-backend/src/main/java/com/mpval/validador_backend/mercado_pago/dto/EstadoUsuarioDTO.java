d96629480202087e373c72271f1009ff7ac6cc33
Santiago Lamot <santiagolamot@hotmail.com>
2025-07-23 15:05:34 -0300

    Update EstadoUsuarioDTO.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/dto/EstadoUsuarioDTO.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/dto/EstadoUsuarioDTO.java
@@ -1,4 +1,12 @@
 package com.mpval.validador_backend.mercado_pago.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class EstadoUsuarioDTO {
+    private String userName;
+    private Boolean licencia;
+    private String vencimientoLicencia;
 }
