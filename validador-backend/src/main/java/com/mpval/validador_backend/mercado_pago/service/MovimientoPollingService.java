b7445a6e7b65c675cc1e09d451361e23c9a8cdf4
Santiago Lamot <santiagolamot@hotmail.com>
2025-07-24 13:52:35 -0300

    Update MovimientoPollingService.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/service/MovimientoPollingService.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/service/MovimientoPollingService.java
@@ -2,6 +2,8 @@ package com.mpval.validador_backend.mercado_pago.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Objects;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +15,8 @@ import org.springframework.web.client.RestTemplate;
 
 import com.mpval.validador_backend.Usuario.entity.Usuario;
 import com.mpval.validador_backend.Usuario.repository.UsuarioRepository;
+import com.mpval.validador_backend.jwt.entity.Token;
+import com.mpval.validador_backend.jwt.repository.TokenRepository;
 import com.mpval.validador_backend.mercado_pago.dto.MovimientoResponse;
 import com.mpval.validador_backend.mercado_pago.entity.Movimiento;
 import com.mpval.validador_backend.mercado_pago.entity.OauthToken;
@@ -27,21 +31,39 @@ public class MovimientoPollingService {
     private final OauthTokenRepository oauthTokenRepository;
     private final NotificacionService notificacionService;
     private final UsuarioRepository usuarioRepository;
+    private final TokenRepository tokenRepository;
 
-    public MovimientoPollingService(RestTemplate r, OauthTokenRepository repo, NotificacionService n, UsuarioRepository u) {
+    public MovimientoPollingService(RestTemplate r, OauthTokenRepository repo, NotificacionService n,
+            UsuarioRepository u, TokenRepository t) {
         this.restTemplate = r;
         this.oauthTokenRepository = repo;
         this.notificacionService = n;
         this.usuarioRepository = u;
+        this.tokenRepository = t;
     }
 
     @Scheduled(fixedRate = 5000)
     public void verificarMovimientos() {
         System.out.println("<<<ENTRA A VERIFICAR MOVIMIENTOS>>>");
-        List<OauthToken> tokens = oauthTokenRepository.findByAccessTokenIsNotNull();
-        for (OauthToken token : tokens) {
+        List<Token> TokensValidos = tokenRepository.findByRevokedFalseAndExpiredFalse();
+
+        LocalDateTime now = LocalDateTime.now();
+
+        List<Usuario> usuariosConSuscripcionActiva = TokensValidos.stream()
+                .map(Token::getUsuario)
+                .filter(usuario -> usuario.getExpiracionSuscripcion() != null &&
+                        usuario.getExpiracionSuscripcion().isAfter(now))
+                .distinct()
+                .collect(Collectors.toList());
+
+        List<OauthToken> oauthTokens = usuariosConSuscripcionActiva.stream()
+                .map(usuario -> oauthTokenRepository.findByUsuario(usuario))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        for (OauthToken token : oauthTokens) {
             Usuario usuario = usuarioRepository.findById(token.getUsuario().getId())
-                .orElseThrow(()-> new RuntimeException("Error al obtener usuario por OauthToken"));
+                    .orElseThrow(() -> new RuntimeException("Error al obtener usuario por OauthToken"));
             String accessToken = token.getAccessToken();
             Long lastId = token.getLastMovementId();
             try {
@@ -76,9 +98,10 @@ public class MovimientoPollingService {
                     }
                 }
             } catch (Exception e) {
-                System.err.println("Error verificando movimientos de usuario " + usuario.getNombreDeUsuario() + ": " + e.getMessage());
+                System.err.println("Error verificando movimientos de usuario " + usuario.getNombreDeUsuario() + ": "
+                        + e.getMessage());
             }
         }
     }
-    
+
 }
