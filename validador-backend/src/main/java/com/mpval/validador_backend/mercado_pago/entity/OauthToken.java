43ab2dd6743e17a0460cabe5c8954522681d2909
Santiago Lamot <santiagolamot@hotmail.com>
2025-07-24 13:52:44 -0300

    Update OauthToken.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/entity/OauthToken.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/entity/OauthToken.java
@@ -12,10 +12,12 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
