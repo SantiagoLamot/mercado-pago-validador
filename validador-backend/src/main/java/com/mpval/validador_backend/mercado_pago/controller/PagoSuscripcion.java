c29bd706d1b916bcaf6c81adda31a92fc17782e7
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-30 15:38:01 -0300

    Update PagoSuscripcion.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/controller/PagoSuscripcion.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/controller/PagoSuscripcion.java
@@ -2,7 +2,6 @@ package com.mpval.validador_backend.mercado_pago.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mpval.validador_backend.mercado_pago.service.MercadoPagoService;
