064833195c5ae7437051105c0f172f96807a217f
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-30 15:38:05 -0300

    Update OauthTokenController.java

--- validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/controller/OauthTokenController.java
+++ validador-backend/src/main/java/com/mpval/validador_backend/mercado_pago/controller/OauthTokenController.java
@@ -3,7 +3,6 @@ package com.mpval.validador_backend.mercado_pago.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
